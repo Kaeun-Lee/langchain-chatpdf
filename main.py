29eff8a9dc09833e5c732c594656d093d4e0fdbc
Kaeun-Lee <lgeun7026@hanmail.net>
2023-08-26 18:39:11 +0900

    add question feature

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from dotenv import load_dotenv
-
-load_dotenv()
-
 from langchain.document_loaders import PyPDFLoader
+from langchain.embeddings import OpenAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-
+from langchain.vectorstores import Chroma
+from langchain.chat_models import ChatOpenAI
+from langchain.retrievers.multi_query import MultiQueryRetriever
+from langchain.chains import RetrievalQA
 
 # Loader
+load_dotenv()
 loader = PyPDFLoader("unsu.pdf")
 pages = loader.load_and_split()  # page별로 split
 
@@ -16,12 +18,19 @@ text_splitter = RecursiveCharacterTextSplitter(
     chunk_size=300,  # 자르는 사이즈
     chunk_overlap=20,  # 겹치는 글자 수를 정해줌
     length_function=len,
-    is_separator_regex=False,  # 정규표현식으로 split할 때 사용
+    is_separator_regex=False,
 )
-
 texts = text_splitter.split_documents(pages)
 
 # Embedding
-from langchain.embeddings import OpenAIEmbeddings
-
 embeddings_model = OpenAIEmbeddings()
+
+# load it into Chroma
+db = Chroma.from_documents(texts, embeddings_model)
+
+# Question
+question = "아내가 먹고 싶어하는 음식은 무엇이야?"
+llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+qa_chain = RetrievalQA.from_chain_type(llm, retriever=db.as_retriever())
+result = qa_chain({"query": question})
+print(result)
